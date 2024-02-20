d7feff71c4aa14b48f15713f7a25589457595e79
ana-msilva <ana.msilva@ibm.com>
2024-02-19 21:52:44 -0300

    feat: primeiro subscribe

--- 1-Rjxs/src/index.ts
+++ 1-Rjxs/src/index.ts
@@ -5,9 +5,13 @@ let source = from(numbers);
 
 function component() {
   source.subscribe({
-    next: (x) => console.log(x),
+    next: (x: number) => console.log(x),
+    error: (e: Error) => console.log(e),
+    complete: ()=> {'Complete'}
+    
     
   });  
 }
 
 component();
+
