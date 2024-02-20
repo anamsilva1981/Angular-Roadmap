6800896d06f0e0d90a2a9845cc29140d50ac5183
ana-msilva <ana.msilva@ibm.com>
2024-02-20 08:43:31 -0300

    criacao

--- 1-Rjxs/src/index.ts
+++ 1-Rjxs/src/index.ts
@@ -1,16 +1,25 @@
-import { from } from "rxjs";
+import { Observable, Subscriber, from } from "rxjs";
 
 let numbers = [1, 5, 10];
 let source = from(numbers);
 
+let sourceInstance = new Observable(Subscriber => {
+  for (let n of numbers){
+    Subscriber.next(n);
+  }
+  Subscriber.complete();
+})
+
+const myObserver = {
+  next: (x: number) => { console.log(x) },
+  error: (e: Error) => console.log(e),
+  complete: ()=> console.log('complete')
+}
+
 function component() {
-  source.subscribe({
-    next: (x: number) => console.log(x),
-    error: (e: Error) => console.log(e),
-    complete: ()=> {'Complete'}
-    
-    
-  });  
+  source.subscribe(myObserver);  
+
+  sourceInstance.subscribe(myObserver)
 }
 
 component();
